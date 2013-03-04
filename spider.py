59acb8d48acff9d2c430b9cdde1166d2bca12149
DingK-R <punkstar0219@gmail.com>
2013-03-05 01:08:18 +0800

    get URL list

--- spider.py
+++ spider.py
@@ -4,6 +4,7 @@
 import urllib
 import time 
 import os
+import re
 
 url = 'http://www.baidu.com';
 def getData(url):
@@ -13,15 +14,28 @@ def getData(url):
     fileName = time.strftime( '%Y-%m-%d_%H:%s',time.localtime( time.time() ) ) + '.html'
     filePath = os.getcwd() + delimiter + fileName
     dirPath = os.getcwd() + delimiter + 'html'
-
+    
     if os.path.exists(dirPath):
-         os.chdir(dirPath)
-         fopen = open( fileName, 'w')
-         fopen.write(data)
-         fp = open(fileName, 'r')
+        os.chdir(dirPath)
+        fopen = open( fileName, 'w')
+        #fopen.write(fileName)
+        #fp = open(data, 'r')
+        #Read file and get url
+        #content = fp.read(fileName);
+        content = data
+        pattern = '<a\s*href=\"(.*?)\"'
+        res = re.compile(pattern)
+        matchs = res.findall(content)
+        if matchs != None:
+            alist = []
+            for found in matchs:
+                if found not in alist:
+                    alist.append(found)
+                print alist
 
-
-         fopen.close
+        #fp.close
+        
+        fopen.close
     else:
         os.mkdir(dirPath)
         print 'make dir'
