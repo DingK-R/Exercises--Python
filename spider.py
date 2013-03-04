db84864064669b89b1ecdc63f59dcb95f27c55ae
DingK-R <punkstar0219@gmail.com>
2013-03-04 18:33:24 +0800

    urllib

--- spider.py
+++ spider.py
@@ -1,10 +1,9 @@
 #! /usr/bin/python
 # Filename: Spider.py
+# -*- coding: UTF-8 -*-
 import urllib
 url = 'http://www.google.com';
 def getData(url):
-    """ 读取google页面
-    """
     print urllib.urlopen(url).read()
 
 getData(url)
