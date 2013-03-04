832d851d34bfb1668d78547ce209835414572fd0
DingK-R <punkstar0219@gmail.com>
2013-03-05 00:04:40 +0800

    fileSystem

--- spider.py
+++ spider.py
@@ -2,8 +2,30 @@
 # Filename: Spider.py
 # -*- coding: UTF-8 -*-
 import urllib
-url = 'http://www.google.com';
+import time 
+import os
+
+url = 'http://www.baidu.com';
 def getData(url):
-    print urllib.urlopen(url).read()
+    delimiter = '/' # should i use const ?
+    data = urllib.urlopen(url).read()
+    
+    fileName = time.strftime( '%Y-%m-%d_%H:%s',time.localtime( time.time() ) ) + '.html'
+    filePath = os.getcwd() + delimiter + fileName
+    dirPath = os.getcwd() + delimiter + 'html'
+
+    if os.path.exists(dirPath):
+         os.chdir(dirPath)
+         fopen = open( fileName, 'w')
+         fopen.write(data)
+         fp = open(fileName, 'r')
+
+
+         fopen.close
+    else:
+        os.mkdir(dirPath)
+        print 'make dir'
+
+
 
 getData(url)
