f069662b3bf6eeb777b6c7aa74e1ba7ef5b20549
DingK-R <punkstar0219@gmail.com>
2013-03-05 01:37:08 +0800

    get URL list

--- spider.py
+++ spider.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 # Filename: Spider.py
 # -*- coding: UTF-8 -*-
+# author: David Ding
 import urllib
 import time 
 import os
@@ -9,33 +10,26 @@ import re
 url = 'http://www.baidu.com';
 def getData(url):
     delimiter = '/' # should i use const ?
-    data = urllib.urlopen(url).read()
-    
-    fileName = time.strftime( '%Y-%m-%d_%H:%s',time.localtime( time.time() ) ) + '.html'
+    fileName = time.strftime( '%Y-%m-%d_%H:%I:%S',time.localtime( time.time() ) ) + '.html'
     filePath = os.getcwd() + delimiter + fileName
     dirPath = os.getcwd() + delimiter + 'html'
     
+    data = urllib.urlopen(url).read()
+    
     if os.path.exists(dirPath):
-        os.chdir(dirPath)
-        fopen = open( fileName, 'w')
-        #fopen.write(fileName)
-        #fp = open(data, 'r')
-        #Read file and get url
-        #content = fp.read(fileName);
-        content = data
         pattern = '<a\s*href=\"(.*?)\"'
         res = re.compile(pattern)
-        matchs = res.findall(content)
+        matchs = res.findall(data)
         if matchs != None:
             alist = []
             for found in matchs:
                 if found not in alist:
                     alist.append(found)
-                print alist
-
-        #fp.close
-        
-        fopen.close
+            
+            os.chdir(dirPath)
+            fopen = open( fileName, 'w')
+            fopen.write(str(alist))
+            fopen.close
     else:
         os.mkdir(dirPath)
         print 'make dir'
