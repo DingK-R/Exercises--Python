113453cded3136828cdcc1688ed5ccd746fdf59a
FreeDKR <punkstar0219@gmail.com>
2014-08-04 10:33:59 +0800

    v3

---
+++ digdigits.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+import sys
+
+class BigDigits():
+
+    def run(self):
+        Digits = self.int_list()
+        try:
+            digits = sys.argv[1]
+            row = 0
+            while row < 7:
+                line = ""
+                column = 0
+                while column < len(digits):
+                    number = int(digits[column])
+                    digit = Digits[number]
+                    line += digit[row] + "  "
+                    column += 1
+                print(line)
+                #return line
+                row += 1
+        except IndexError:
+            print("usage: digdigits.py <number>")
+        except ValueError as err:
+            print(err, "in", digits)
+
+    def int_list(self):
+        zero = ["  ***  ", " *   * ", "*     *", "*     *", "*     *", " *   * ", "  ***  "]
+
+        one = [" * ", "** ", " * ", " * ", " * ", " * ", "***"]
+
+        two = ["  ***  ", " *   * ", "*     *", "     * ", "    *  ", "  *    ", "*******"]
+
+        three = ["*******", "      *", "      *", "*******", "      *", "      *", "*******"]
+
+        four = ["    *  ", "   *   ", "  *  * ", " *   * ", "*******", "     * ", "     * "]
+
+        five = ["*******", "*      ", "*      ", "*******", "      *", "      *", "*******"]
+
+        six = ["*******", "*      ", "*      ", "*******", "*     *", "*     *", "*******"]
+
+        seven = ["*******", "      *", "      *", "      *", "      *", "      *", "      *"]
+
+        eight = ["*******", "*     *", "*     *", "*******", "*     *", "*     *", "*******"]
+
+        nine = ["*******", "*     *", "*     *", "*******", "      *", "      *", "*******"]
+
+        return [zero, one, two, three, four, five,six,seven,eight,nine]
+
+
+
+BigDigits().run()
