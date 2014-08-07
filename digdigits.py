35feea84f5f28fc7e42159269989f8ff9344bbe0
FreeDKR <punkstar0219@gmail.com>
2014-08-07 10:44:43 +0800

    print digital

--- digdigits.py
+++ digdigits.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python3
 import sys
 
-"""
-love@LOVE:~/python3$ ./digdigits.py 12345
- *     ***    *******      *    *******
-**    *   *         *     *     *
- *   *     *        *    *  *   *
- *        *   *******   *   *   *******
- *       *          *  *******        *
- *     *            *       *         *
-***  *******  *******       *   *******
-"""
+
 class BigDigits():
 
     def run(self):
-        Digits = self.int_list()
+        # self.Digits = self.int_list()
+        self.Digits = self.num_list()
         try:
             digits = sys.argv[1]
             row = 0
@@ -23,11 +15,10 @@ class BigDigits():
                 column = 0
                 while column < len(digits):
                     number = int(digits[column])
-                    digit = Digits[number]
+                    digit = self.Digits[number]
                     line += digit[row] + "  "
                     column += 1
                 print(line)
-                #return line
                 row += 1
         except IndexError:
             print("usage: digdigits.py <number>")
@@ -57,6 +48,17 @@ class BigDigits():
 
         return [zero, one, two, three, four, five,six,seven,eight,nine]
 
+    def num_list(self):
+        new_list = []
+        n = []
+        for num in range(10):
+            _list = BigDigits.int_list(self)[num]
+            for i in _list:
+                new_list.append(i.replace('*', str(num)))
 
+        for i in range(0, len(new_list) + 1, 7):
+            n.append(new_list[i:i+7])
+
+        return n
 
 BigDigits().run()
